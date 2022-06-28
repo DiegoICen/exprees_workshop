2028e7906247bec6d647c43b9c324321f4c26097
DiegoICen <diego.i.cen.glez@gmail.com>
2022-06-28 14:26:17 -0500

    "Declare de forma dinamica, saludos"

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ app.get("/", (req /*Toda la ifo se almacenara en req*/,
 res/*la respuesta que nosotros vamos a dar*/, 
 next) =>{
     res.status(200);
-    res.send("Bienvenido al servidor");
+    res.send("Bienvenido al servidor perro");
+})
+
+app.get("/:name", (req, res, next) => {
+    console.log(req.params.name);
+    res.status(200);
+    res.send("Esta en la pagina de " + req.params.name)
 })
 
 /*
-Verbos HTTP
+Verbos HTTP, Los mas usados son: GET,POST,PATCH,PUT,DELETE*/
 
-Los mas usados son: GET,POST,PATCH,PUT,DELETE*/
-
-app.listen(3000,() => {
+app.listen(process.env.PORT || 3000,() => {
     console.log("SERVER IS RUNNING")
 }); //(Primero se pone el perto, luego una funcion flecha );
